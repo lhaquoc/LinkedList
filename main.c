e69e46bc324676cc59bf01bd1a1f5c7c81b58bca
lhaquoc <38935979+lhaquoc@users.noreply.github.com>
2021-02-04 16:55:12 +0700

    add functions

--- main.c
+++ main.c
@@ -31,16 +31,34 @@ node_t *insert_at_head(node_t *head, node_t *node_to_insert) {
     return node_to_insert;
 }
 
+node_t *find_a_node(node_t *head, int value) {
+    node_t *temp = head;
+    while (temp != NULL)
+    {
+        if(temp->value == value)
+            return temp;
+        temp = temp->next;
+    }
+    return NULL;
+}
+void insert_after_node(node_t *node_to_insert_after, node_t *new_node) {
+    new_node->next = node_to_insert_after->next;
+    node_to_insert_after->next = new_node;
+}
 int main(void) {
     node_t *head = NULL;
-    node_t *tmp;
+    node_t *temp;
 
     for(int i = 0; i < 25; i++) {
-        tmp = create_new_node(i);
-        head = insert_at_head(head, tmp);
+        temp = create_new_node(i);
+        head = insert_at_head(head, temp);
     }
 
-    print_list(head);
+    temp = find_a_node(head, 13);
+    printf("Found node with value %d\n", temp->value);
+    
+    insert_after_node(temp, create_new_node(99));
+
     print_list(head);
     return 0;
 }
